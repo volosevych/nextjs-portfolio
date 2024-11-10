2f1a6a8bb47527c50e7a817f0c79a7b55e572a76
volosevych <volosevichh@gmail.com>
2024-11-09 22:46:58 -0700

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/nextjs-portfolio',
+    assetPrefix: '/nextjs-portfolio/',
+};
 
 export default nextConfig;
+
+  
